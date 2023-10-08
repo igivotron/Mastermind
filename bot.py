92dc28400259711e4c03a0b93e27a957b3e89b50
Igiv Igiv <gregoire.igor@gmail.com>
2023-10-08 11:36:41 +0200

    upgrade

---
+++ bot.py
@@ -0,0 +1,13 @@
+from sympy.utilities.iterables import multiset_permutations
+from rules import MasterMind
+
+class Bot(MasterMind):
+    def __init__(self):
+        pass
+
+    def gen_Permutation(self):
+        pass
+
+robocop = Bot()
+robocop.gen_Permutation()
+robocop.sequence()
